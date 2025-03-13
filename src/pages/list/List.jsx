120ec60d11cbdd27d0139811803dce81240dd15a
Mayur0018 <mayurnish18@gmail.com>
2025-03-13 22:01:55 +0530

    commit

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
-
+import React from "react";
+import Sidebar from "../../Components/sidebar/Sidebar";
+import Navbar from "../../Components/navbar/Navbar";
+import Database from "../../Components/datatable/Datatable";
+import "./list.scss"
 export default function List() {
   return (
-    <div>List</div>
-  )
+    <div className="list">
+      <Sidebar />
+      <div className="listContainer">
+        <Navbar/>
+        <Database />
+      </div>
+    </div>
+  );
 }
