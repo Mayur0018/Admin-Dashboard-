1b825586da2aaee684cba86cd94d3fab862aef39
Mayur0018 <mayurnish18@gmail.com>
2025-03-15 11:34:23 +0530

    commit

---
+++ src/FormSource.jsx
@@ -0,0 +1,47 @@
+const UserInputs = [
+    {
+      id: "username",
+      label: "Username",
+      type: "text",
+      placeholder: "john_doe123"
+    },
+    {
+      id: "email",
+      label: "Email Address",
+      type: "email",
+      placeholder: "john.doe@example.com"
+    },
+    {
+      id: "password",
+      label: "Password",
+      type: "password",
+      placeholder: "********"
+    },
+    {
+      id: "dob",
+      label: "Date of Birth",
+      type: "date",
+      placeholder: ""
+    },
+    {
+      id: "phone",
+      label: "Phone Number",
+      type: "tel",
+      placeholder: "+1 987 654 3210"
+    },
+    {
+      id: "address",
+      label: "Address",
+      type: "text",
+      placeholder: "123 Main St, City, Country"
+    },
+    {
+      id: "bio",
+      label: "Short Bio",
+      type: "textarea",
+      placeholder: "Tell us about yourself"
+    }
+  ];
+  
+  export default UserInputs;
+  
